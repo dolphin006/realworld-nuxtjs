63e267007a697be02dc00347f949c6ccdc5bfff9
LAPTOP-556JJHHA\dolphin <sdolphi006@163.com>
2022-02-15 13:25:05 +0800

    init

---
+++ api/article.js
@@ -0,0 +1,80 @@
+import { request } from '@/plugins/request'
+
+// 获取公共文章列表
+export const getArticles = params => {
+  return request({
+    method: 'GET',
+    url: '/api/articles',
+    params
+  })
+}
+
+// 获取公共文章列表
+export const getYourFeedArticles = params => {
+  return request({
+    method: 'GET',
+    url: '/api/articles/feed',
+    params,
+    // headers: {
+    //   // 添加用户身份，数据格式：Token空格Token数据
+    //   Authorization: `Token eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NDgxMTYsInVzZXJuYW1lIjoibHB6OTk5IiwiZXhwIjoxNTk3NzQxNTA4fQ.2yO8Fss4hYnvsIN2UYHsutQ1hmYqSSAA-UrIRnP4DOY`
+    // }
+  })
+}
+
+// 添加点赞
+export const addFavorite = slug => {
+  return request({
+    method: 'POST',
+    url: `/api/articles/${slug}/favorite`
+  })
+}
+
+// 取消点赞
+export const deleteFavorite = slug => {
+  return request({
+    method: 'DELETE',
+    url: `/api/articles/${slug}/favorite`
+  })
+}
+
+// 获取文章详情
+export const getArticle = slug => {
+  return request({
+    method: 'GET',
+    url: `/api/articles/${slug}`
+  })
+}
+
+// 获取文章评论
+export const getComments = slug => {
+  return request({
+    method: 'GET',
+    url: `/api/articles/${slug}/comments`
+  })
+}
+
+/**
+ * 创建文章
+ * @param {*} data 
+ */
+export const createArticles = data => (
+  request({
+    url: "/api/articles",
+    data,
+    method: 'POST'
+  })
+)
+
+/**
+ * 更新文章
+ * @param {string} slug 文章唯一id
+ * @param {*} data 文章内容
+ */
+export const putArticle = (slug, data) => (
+  request({
+    url: `/api/articles/${slug}`,
+    method: 'PUT',
+    data
+  })
+)
